1c38d54b2381c407a0ea5b30fa1e9f8e3c7c3611
Manfred Rosskamp <manfred.rosskamp@gmx.de>
2020-06-03 16:21:43 +0200

    make PWA

--- MyCastle/wwwroot/Garten.js
+++ MyCastle/wwwroot/Garten.js
@@ -145,3 +145,19 @@ gartenSvg.addEventListener("load", function () {
 
 }, false);
 
+
+window.addEventListener('load', e => {
+  registerSW(); 
+});
+
+async function registerSW() { 
+  if ('serviceWorker' in navigator) { 
+    try {
+      await navigator.serviceWorker.register('./sw.js'); 
+    } catch (e) {
+      alert('ServiceWorker registration failed. Sorry about that.'); 
+    }
+  } else {
+    document.querySelector('.alert').removeAttribute('hidden'); 
+  }
+}
---
+++ MyCastle/wwwroot/sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+	'./',
+	'./index.html',
+	'./favicon.ico',
+	'./Garten.css',
+	'./Garten.js',
+	'./Garten.svg',
+	'./settings.json'
+];
+
+self.addEventListener('install', async event => {
+	const cache = await caches.open(cacheName);
+	await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+	const req = event.request;
+	event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+	const cache = await caches.open(cacheName);
+	const cachedResponse = await cache.match(req);
+	return cachedResponse || fetch(req);
+}
