58116ab71487c52719375caaa2288f1e904ae970
Manfred Rosskamp <manfred.rosskamp@mum.de>
2022-06-08 11:28:56 +0200

    remove service worker

--- MyCastle/wwwroot/Garten.js
+++ MyCastle/wwwroot/Garten.js
@@ -230,23 +230,3 @@ var app = new Vue({
 		this.loadPrograms();
 	}
 });
-
-
-
-
-
-window.addEventListener('load', e => {
-  registerSW(); 
-});
-
-async function registerSW() { 
-  if ('serviceWorker' in navigator) { 
-    try {
-      await navigator.serviceWorker.register('./sw.js'); 
-    } catch (e) {
-      alert('ServiceWorker registration failed. Sorry about that.'); 
-    }
-  } else {
-    document.getElementById('alertServiceWorker').removeAttribute('hidden'); 
-  }
-}
